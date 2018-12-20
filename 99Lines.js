206cb05c34110de7b7926978e355df0f562fd417
Mitch Robinson <mitchellallenrobinson@gmail.com>
2018-12-19 19:18:52 -0600

    Changes made to .js and html title.

--- 99Lines.js
+++ 99Lines.js
@@ -1,3 +1,8 @@
-let arrFriends = ['Jake','Clint','Sara','Wynn','Chip'];
+const myFriends = ['Jake', 'Clint', 'Sara', 'Wynn', 'Chip'];
 
-console.log (arrFriends);
+let i = 99;
+let total = 100;
+
+while (i < total, i){
+    console.log (+ i + " " + "lines of code in the file,"+ " " + i + " " + "lines of code;" + " " + myFriends + " " + "strikes one out, clears it all out," + " " + --i + " " + "lines of code in the file");
+}; 
